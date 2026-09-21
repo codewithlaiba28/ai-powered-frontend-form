d329b8df39bec9a9c1d3e8f2b76f21d883e3f5ea
codewithlaiba28 <codewithlaiba28@gmail.com>
2026-09-21 15:47:12 +0500

    add registration page component

---
+++ src/app/register/page.tsx
@@ -0,0 +1,9 @@
+import RegistrationForm from '@/components/RegistrationForm';
+
+export default function RegisterPage() {
+  return (
+    <main className="flex-1 flex flex-col justify-center py-6">
+      <RegistrationForm />
+    </main>
+  );
+}
